94e905b0c9ae69e7db611a0121922e23588b74fc
AbdiasAlpire <abdias.alpire@assuresoft.com>
2025-10-22 09:24:44 -0400

    chore: refactor stuff

--- src/tests/login/TC002.spec.ts
+++ src/tests/login/TC002.spec.ts
@@ -8,8 +8,8 @@ test('TC002: Verify login with valid account', async ({ page, loginPage }) => {
   const userEmail = process.env.USER_EMAIL as string;
   const userPassword = process.env.USER_PASSWORD as string;
   await loginPage.goto();
-  await loginPage.fillUsername(userEmail);
-  await loginPage.fillPassword(userPassword);
+  await loginPage.fillUsernameInput(userEmail);
+  await loginPage.fillPasswordInput(userPassword);
   await loginPage.clickLoginButton();
   const header = new Header(page);
   await expect(page).toHaveURL('/');
