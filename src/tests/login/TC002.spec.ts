61d6aa28043233ecc970d476437e9e076075c72f
Oswi <edwin.roman@ucb.edu.bo>
2025-10-21 22:38:56 -0400

    chore: deleting blank spaces

--- src/tests/login/TC002.spec.ts
+++ src/tests/login/TC002.spec.ts
@@ -7,14 +7,11 @@ dotenv.config();
 test('TC002: Verify login with valid account', async ({ page, loginPage }) => {
   const userEmail = process.env.USER_EMAIL as string;
   const userPassword = process.env.USER_PASSWORD as string;
-
   await loginPage.goto();
-
   await loginPage.fillUsername(userEmail);
   await loginPage.fillPassword(userPassword);
   await loginPage.clickLoginButton();
   const header = new Header(page);
-
   await expect(page).toHaveURL('/');
   await expect(header.avatarElement).toHaveText(userEmail.charAt(0).toUpperCase());
 });
