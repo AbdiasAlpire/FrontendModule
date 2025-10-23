aa53e4ed31910f2c82b35b31ef42866cfb3b8ca0
carolylazo <carol.lazo@assuresoft.tech>
2025-10-23 14:05:04 -0400

    Feat: creating the test

--- src/tests/customer/TC021.spec.ts
+++ src/tests/customer/TC021.spec.ts
@@ -1,11 +1,20 @@
-import { mergeTests } from "@playwright/test";
-import {
-  test as customerTest,
-  expect,
-} from "../../fixtures/CustomerPageFixture";
+import { mergeTests, expect } from "@playwright/test";
 import { test as loggedInTest } from "../../fixtures/LoggedInFixture";
+import { test as customerTest } from "../../fixtures/CustomerPageFixture";
 import * as dotenv from "dotenv";
 
 const test = mergeTests(loggedInTest, customerTest);
 
 dotenv.config();
+
+test("TC021: Verify a new customer can be created by type people", async ({
+  customerPage,
+  loginPage,
+}) => {
+  await customerPage.goto();
+  await customerPage.clickAddNewCustomer();
+  await customerPage.clickTypeDropDown();
+  await customerPage.selectPeopleAsType();
+  await customerPage.clickSearchBox();
+  await customerPage.fillSearchBox("Search");
+});
