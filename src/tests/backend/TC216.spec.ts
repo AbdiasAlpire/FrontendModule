32cc2d1a57675f39eb5992553395cebc42289389
carolylazo <carol.lazo@assuresoft.tech>
2025-10-28 08:24:46 -0400

    chore: change weatherDDT.json name to historicaldata.json

--- src/tests/backend/TC216.spec.ts
+++ src/tests/backend/TC216.spec.ts
@@ -5,7 +5,10 @@ import { config } from "../../utils/config/config";
 import fs from "fs";
 import path from "path";
 
-const dataPath = path.resolve(__dirname, "../../utils/data/weatherDDT.json");
+const dataPath = path.resolve(
+  __dirname,
+  "../../utils/data/historicaldata.json"
+);
 const jsonData = fs.readFileSync(dataPath, "utf-8");
 const testData = JSON.parse(jsonData);
 
