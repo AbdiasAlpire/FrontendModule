d0b6dbaf9b3fbee109cb8b1dfd1e6fa12f7af81a
Oswi <edwin.roman@ucb.edu.bo>
2025-10-28 20:16:03 -0400

    chore: added vailadations for response bodies

--- src/tests/backend/TC211.spec.ts
+++ src/tests/backend/TC211.spec.ts
@@ -11,17 +11,31 @@ test.describe('WeatherStack API - Forecast', () => {
     await api.init();
   });
 
-  test('GET forecast with empty query should return 400 Bad Request', async () => {
+  test('GET forecast empty query should returns 400 and expected error response body', async () => {
     const params = {
       access_key: config.accessKey,
       query: '',
     };
 
     const response = await api.get(endpoints.weather.forecast, params);
+    const data = await response.json();
 
     expect(response.status()).toBe(400);
 
-    console.log(`Status code: ${response.status()} (Expected: 400 Bad Request)`);
+    const expectedBody = {
+      success: false,
+      error: {
+        code: 601,
+        type: 'missing_query',
+        info: 'Please specify a valid location identifier using the query parameter.',
+      },
+    };
+
+    expect(data).toMatchObject(expectedBody);
+
+    console.log(
+      `Status code: ${response.status()} | error.code=${data.error?.code} | error.type=${data.error?.type} | error.info="${data.error?.info}"`
+    );
   });
 
   test.afterAll(async () => {
--- src/tests/backend/TC229.spec.ts
+++ src/tests/backend/TC229.spec.ts
@@ -11,17 +11,31 @@ test.describe('WeatherStack API - Forecast', () => {
     await api.init();
   });
 
-  test('GET forecast only blank spaces query should return 400 Bad Request', async () => {
+  test('GET forecast only blank spaces query returns 400 and expected reponse body', async () => {
     const params = {
       access_key: config.accessKey,
       query: '      ',
     };
 
     const response = await api.get(endpoints.weather.forecast, params);
+    const data = await response.json();
 
     expect(response.status()).toBe(400);
 
-    console.log(`Status code: ${response.status()} (Expected: 400 Bad Request)`);
+    const expectedBody = {
+      success: false,
+      error: {
+        code: 615,
+        type: 'request_failed',
+        info: 'Your API request failed. Please try again or contact support.',
+      },
+    };
+
+    expect(data).toMatchObject(expectedBody);
+
+    console.log(
+      `Status code: ${response.status()} | error.code=${data.error?.code} | error.type=${data.error?.type} | error.info="${data.error?.info}"`
+    );
   });
 
   test.afterAll(async () => {
